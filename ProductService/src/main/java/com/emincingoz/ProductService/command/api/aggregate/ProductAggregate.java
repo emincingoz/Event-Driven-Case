b5862be3a868c157d25240d373a0ba815f6a43d7
Emin Cingöz <emincingoz37@gmail.com>
2023-02-05 00:27:11 +0300

    ProductService Command api initial commit

---
+++ ProductService/src/main/java/com/emincingoz/ProductService/command/api/aggregate/ProductAggregate.java
@@ -0,0 +1,43 @@
+package com.emincingoz.ProductService.command.api.aggregate;
+
+import com.emincingoz.ProductService.command.api.commands.CreateProductCommand;
+import com.emincingoz.ProductService.command.api.events.ProductCreatedEvent;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+
+@Aggregate
+public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public ProductAggregate() {
+    }
+
+    public ProductAggregate(CreateProductCommand createProductCommand) {
+        // You can perform all the validations.
+
+        // Also you can use builder...
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+
+        AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        this.quantity = productCreatedEvent.getQuantity();
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.name = productCreatedEvent.getName();
+    }
+}
